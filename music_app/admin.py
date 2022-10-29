6081a57e8e68d9f64bb308a369cc479cc6f09cb9
tobienitan <enitanoluwatobi@gmail.com>
2022-10-29 09:25:22 +0100

    initial commit

--- music_app/admin.py
+++ music_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Artiste,song,lyric
 
+admin.site.register(Artiste)
+admin.site.register(song)
+admin.site.register(lyric)
 # Register your models here.
--- music_app/models.py
+++ music_app/models.py
@@ -1,3 +1,32 @@
+from email import contentmanager
+from email.policy import default
+from operator import mod
+from tkinter import CASCADE
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    
+
+class song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artisteId = models.IntegerField()
+    
+
+class lyric(models.Model):
+    song = models.ForeignKey(song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    songId = models.IntegerField()
+   
+    
+
